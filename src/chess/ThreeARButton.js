dce7577ceee4b42573e1167eff09b397252894ad
Alexandr <sao.bur@yandex.ru>
2022-06-08 15:53:26 +0300

    update deploy check

--- src/chess/ThreeARButton.js
+++ src/chess/ThreeARButton.js
@@ -16,6 +16,7 @@ class ARButton {
           'http://www.w3.org/2000/svg',
           'svg',
         )
+
         svg.setAttribute('width', 38)
         svg.setAttribute('height', 38)
         svg.style.position = 'absolute'
